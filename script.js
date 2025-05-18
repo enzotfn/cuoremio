f21ff86680d4152b113ad628a0a02344706487b7
enzotfn <enzo.tofani54@gmail.com>
2025-05-18 19:15:44 +0200

    finito la baso

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ function toggleDetails(container) {
     const content = container.querySelector('.content');
     content.style.display = content.style.display === 'block' ? 'none' : 'block';
 }
+
+const compliments = [
+    "Tu es incroyable.",
+    "Ton sourire illumine la journée.",
+    "Tu rends tout plus beau autour de toi.",
+    "Je t'aime",
+    "Tu es une personne exceptionnelle.",
+    "J’adore passer du temps avec toi.",
+    "Il mio cuore.",
+    "Ti amo.",
+    "I love you.",
+    "Tu es magnifique.",
+    "Tu es la meilleure chose qui me soit arrivée.",
+    "Je suis pressé de te revoir.",
+    "Je suis pressé d'aller loin avec toi."
+];
+
+document.getElementById('complimentBtn').addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * compliments.length);
+    document.getElementById('complimentText').textContent = compliments[randomIndex];
+});
